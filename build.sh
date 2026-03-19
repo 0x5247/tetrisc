0ee2b8fe9f5530a086139a9fe8b97eaae9dc4b6c
Arjit Geo <arjitgeo@gmail.com>
2026-03-19 19:16:49 +0530

    fixed build.sh

--- build.sh
+++ build.sh
@@ -1,36 +1,7 @@
 #!/usr/bin/env bash
 
-catch() {
-	if (( $? != 0 )); then
-		echo "error:" $1 >&2
-		exit
-	fi
-}
+llvm-mc -triple=riscv32-unknown-elf -mattr=+c,+zmmul,+Zcb -filetype=obj -show-encoding ./src/main.s -o main.o && \
 
-cleanup() {
-	fcleanup $old_files
-}
-
-old_files=$(find . -maxdepth 1 -type f -printf '%P ')
-trap cleanup EXIT INT TERM
-
-llvm-mc -triple=riscv32-unknown-elf -mattr=+c,+zmmul,+Zcb -filetype=obj -show-encoding ./src/main.s -o main.o
-catch "compilation failed"
-
-ld.lld -m elf32lriscv -e _start main.o; catch "linking failed"
-
-if [ "$1" = "-d" ]; then
-	llvm-objdump --arch-name=riscv32 --mattr=+c,+zmmul,+Zcb -M no-aliases -d
-	exit
-fi
-
-clear
-
-if [ "$1" = "-x" ]; then
-	qemu-riscv32 a.out | xxd
-	exit
-fi
+ld.lld -m elf32lriscv -e _start main.o && \
 
 qemu-riscv32 a.out
-echo "exit code $?"
-
